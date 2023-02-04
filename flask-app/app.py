e4983aaffca5b5ba9dda77c3d5f8cfc69a1d7d64
Vyankatesh Nandapurkar <vnandapu@ucsc.edu>
2023-02-04 11:19:38 -0800

    basic 3-page server

--- flask-app/app.py
+++ flask-app/app.py
@@ -6,3 +6,13 @@ app = Flask(__name__) # register current .py file as the module/app name
 def index():
     # entrypoint to our website
     return render_template("index.html")
+
+@app.route('/welcome')
+def welcome():
+    # this page will get routed to when index.html is done. This is our input page.
+    return render_template("main.html")
+
+@app.route('/results')
+def results():
+    # this will get routed to when we are ready to display results-
+    return render_template("results.html")
