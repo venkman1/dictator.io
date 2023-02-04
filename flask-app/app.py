a9e40f45b1ba96e29a8e74a1565ad416685f682d
Vyankatesh Nandapurkar <vnandapu@ucsc.edu>
2023-02-04 10:58:24 -0800

    serving index.html

--- flask-app/app.py
+++ flask-app/app.py
@@ -3,6 +3,6 @@ from flask import Flask, render_template
 app = Flask(__name__) # register current .py file as the module/app name
 
 @app.route('/')
-def hello():
+def index():
     # entrypoint to our website
     return render_template("index.html")
